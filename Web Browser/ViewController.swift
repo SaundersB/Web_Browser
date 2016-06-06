5235fedf19cdda5389546412969c608e372e05d8
SaundersB <speakeasys@gmail.com>
2016-06-05 17:28:05 -0700

    Adding a new mode: 1. When the user inputs a URL without ".com", the input is searched with Google. 2. If the user does have ".com", then we directly go to that link. 3. If the user does not input "http://", then it is input for the user.

--- Web Browser/ViewController.swift
+++ Web Browser/ViewController.swift
@@ -12,7 +12,6 @@ import UIKit
 class ViewController: UIViewController, UISearchBarDelegate {
     
     @IBOutlet weak var webView: UIWebView!
-    
     @IBOutlet weak var navigation_bar: UINavigationBar!
     @IBOutlet weak var navigation_bar_button: UINavigationItem!
     @IBOutlet var back_button: NSObject!
@@ -21,19 +20,46 @@ class ViewController: UIViewController, UISearchBarDelegate {
 
     @IBOutlet weak var address_field: UITextField!
     
+    var myProgressView: UIProgressView! = nil
+    var myTimer: NSTimer!
+    var theBool: Bool!
+    
     @IBAction func load_website(sender: AnyObject) {
-        //self.searchBar.text = "http://news.ycombinator.com/"
-        //let text = self.address_field.text!
-        let text = "http://news.ycombinator.com/"
-        let url = NSURL(string: text)
-        let req = NSURLRequest(URL:url!)
-        self.webView!.loadRequest(req)
-        self.webView.scalesPageToFit = true
+        var text = ""
+        
+        if(self.address_field.text != ""){
+            // Verify that the address has the necessary http://
+            if (self.address_field.text!.containsString("http://") || self.address_field.text!.containsString("https://")) {
+                text = self.address_field.text!
+            }
+            else {
+                print("Missing http://. Adding it now.")
+                text = "http://"
+            }
+            // If a web address was not entered, we'll run it as a google search.
+            if (!self.address_field.text!.containsString(".com")){
+                print("Text is missing .com. Running as a Google search")
+                let google_search_url = "www.google.com/#q="
+                text = text + google_search_url + self.address_field.text!
+                print("Sending URL: " + text)
+            }
+            else {
+                print("Text does have .com")
+                text = text + self.address_field.text!
+            }
+            
+            let url = NSURL(string: text)
+            let req = NSURLRequest(URL:url!)
+            self.webView!.loadRequest(req)
+            self.webView.scalesPageToFit = true
+            self.webView.allowsInlineMediaPlayback = true
+            self.webView.allowsPictureInPictureMediaPlayback = true
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //self.searchBar.delegate = self
+        loadDefaultWebsite()
     }
     
     @IBAction func backAction(sender: AnyObject) {
@@ -55,4 +81,17 @@ class ViewController: UIViewController, UISearchBarDelegate {
     @IBAction func stopAction(sender: AnyObject) {
         webView.stopLoading()
     }
+    
+    @IBAction func loadHomePage(sender: AnyObject) {
+        loadDefaultWebsite()
+    }
+    
+    func loadDefaultWebsite() {
+        let text = "http://news.ycombinator.com/"
+        let url = NSURL(string: text)
+        let req = NSURLRequest(URL:url!)
+        self.webView!.loadRequest(req)
+        self.webView.scalesPageToFit = true
+    }
+    
 }
